04601ae68dc055bdd114ada5c4ad9cb1b7d43560
Jacob Epstein <jepstein@umass.edu>
2024-06-12 10:02:09 -0400

    isolation trees test job

--- isolation_trees.py
+++ isolation_trees.py
@@ -9,8 +9,12 @@ from sklearn.ensemble import IsolationForest
 import pickle as pkl
 import os
 
+# TODO:
+# - extend this to work for all states and species (will be easy when data cleaning is done)
+# - calculate the evaluation metrics, save those results
+
 print(os.getcwd())
-df = pd.read_csv("all data/citizen/kerala.csv")
+df = pd.read_csv("/rprojectnb/sparkpit/pit-seasonwatch/pitne-season-watch/all data/citizen/kerala.csv")
 df = df[df["Species_name"] == "Mango (all varieties)-Mangifera indica"]
 df = df.drop(["Date_of_observation", "Observation_ID", "User_id", "User_Tree_id"], axis=1)
 df = df[df["Year"] == 2023]
@@ -23,6 +27,8 @@ try:
     print("making the directory")
 except:
     print(f"models/kerala/2023 already exists")
+
+ref_data = []
 for week in range(1):
     df_week = df_noNaN[df_noNaN['Week'] == week]
     model = IsolationForest(n_estimators = 100, verbose = 1, random_state = 42)
@@ -32,8 +38,21 @@ for week in range(1):
         os.makedirs(f"models/kerala/2023/week{week}")
     except:
         print(f"models/kerala/2023/week{week} already exists")
-    with open(f"models/kerala/2023/week{week}/mango.pkl", 'wb') as file:
+    with open(f"models/kerala/2023/week{week}/isolation_mango.pkl", 'wb') as file:
         # Serialize and save the data to the file
         pkl.dump(model, file)
-    print("done!")
+
+    # get dataframe of inliers
+    inlier_indices = [index for index, value in enumerate(preds) if value == 1]
+    df_week_inliers = df_week.iloc[inlier_indices]
+    outlier_indices  = [index for index, value in enumerate(preds) if value == -1]
+    df_week_outliers = df_week.iloc[outlier_indices]
     
+    print("outlier scores: {}".format(model.decision_function(df_week_outliers)))
+    print("inlier scores: {}".format(model.decision_function(df_week_inliers)))
+    
+    # get row with max decision score
+    max_idx = np.argmax(model.decision_function(df_week_inliers))
+    max_row = df_week_inliers.iloc[max_idx]
+    
+    print("best row: {}".format(max_row))
